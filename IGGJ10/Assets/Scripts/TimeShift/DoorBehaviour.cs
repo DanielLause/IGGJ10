209e49dc4b16e25609a7368d76ae4fce9dba3256
Daniel Lause <info@daniel-lause.de>
2016-10-29 00:38:17 +0200

    NEW: Door Prefab NEW: DoorBlocked bool

--- IGGJ10/Assets/Scripts/TimeShift/DoorBehaviour.cs
+++ IGGJ10/Assets/Scripts/TimeShift/DoorBehaviour.cs
@@ -7,8 +7,17 @@ using UnityEngine;
 [RequireComponent(typeof(SphereCollider))]
 class DoorBehaviour : TimeShiftManager
 {
+    public bool DoorBlocked = false;
+    public float TriggerRadius;
+    private SphereCollider collider;
+    void Start()
+    {
+        collider = GetComponent<SphereCollider>();
+        collider.radius = TriggerRadius;
+    }
     void Update()
     {
+        if (!DoorBlocked)
         OnUpdate();
     }
 
